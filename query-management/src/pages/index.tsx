563971e821873bf7096a2f901bf167faf3f6b6de
Katrina Connors <koktakatrina@gmail.com>
2025-05-03 22:39:05 -0700

    front end render logic

--- query-management/src/pages/index.tsx
+++ query-management/src/pages/index.tsx
@@ -1,94 +1,126 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { useMemo, useState, useEffect } from 'react';
+import Image from 'next/image'
+import styles from './page.module.css'
+import { useMemo, useState, useEffect } from 'react'
 import {
   MantineReactTable,
   useMantineReactTable,
   type MRT_ColumnDef,
-} from 'mantine-react-table';
+} from 'mantine-react-table'
 
-import { Text, Modal, Stack, Button, Tooltip } from '@mantine/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faExclamation, faPlus } from '@fortawesome/free-solid-svg-icons'
-
+import { Text, Modal, Stack, Button, Tooltip } from '@mantine/core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCheck,
+  faExclamation,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons'
 
 type Info = {
-  question: string;
-  answer: string;
-  queries: string;
-};
-
-
+  question: string
+  answer: string
+  query?: {
+    id: string
+    title: string
+    description?: string
+    createdAt: string
+    updatedAt: string
+    status: string
+  }
+}
 type FormDataResponse = {
   data: {
-    formData: Info[];
-  };
-};
+    formData: Info[]
+  }
+}
 
 export default function Home() {
   //should be memoized or stable
-  const [opened, setOpened] = useState(false);
-  const [selectedRow, setSelectedRow] = useState<Info | null>(null);
-  const [form,setForm] = useState<Info[]>([]);
+  const [opened, setOpened] = useState(false)
+  const [selectedRow, setSelectedRow] = useState<Info | null>(null)
+  const [form, setForm] = useState<Info[]>([])
 
   useEffect(() => {
     fetch('http://127.0.0.1:8080/form-data')
-      .then((res) => res.json())
+      .then(res => res.json())
       .then((data: FormDataResponse) => {
-        const questionAnswer = data.data.formData;
-        setForm(questionAnswer);
+        const questionAnswer = data.data.formData
+        setForm(questionAnswer)
       })
-      .catch(console.error);
-  }, []);
+      .catch(console.error)
+  }, [])
 
-  
-  const columns = useMemo<MRT_ColumnDef<Info>[]>(() => [
+  const columns = useMemo<MRT_ColumnDef<Info>[]>(
+    () => [
       {
-        accessorKey:'question', 
+        accessorKey: 'question',
         header: 'Question',
       },
       {
         accessorKey: 'answer',
         header: 'Answer',
       },
-   
+
       {
         header: 'Queries',
-        Cell: ({ row }) => (<div style= {{backgroundColor:'#d4fcd4',
-          width: '100%',
-          height: '100%',
-          padding: '0.5rem',
-          boxSizing: 'border-box',}}>
-            
-          <button >
-          <FontAwesomeIcon icon={faPlus} />
-          </button>
-          <button >
-          <FontAwesomeIcon icon={faCheck} />
-          </button>
-          <br></br>
-          <Tooltip label="View/update query">
-          <button onClick={
-            () => {
-              setSelectedRow(row.original);
-              setOpened(true);
-            }
-          }>
-          <FontAwesomeIcon icon={faExclamation} />
-          </button>
-    </Tooltip>
-          
+        Cell: ({ row }) => (
+          <div
+            style={{
+              backgroundColor: '#d4fcd4',
+              width: '100%',
+              height: '100%',
+              padding: '0.5rem',
+              boxSizing: 'border-box',
+            }}
+          >
+            {row.original.query && row.original.query.status === 'OPEN' ? (
+              <Tooltip label="View/update open query">
+                <button
+                  onClick={() => {
+                    setSelectedRow(row.original)
+                    setOpened(true)
+                  }}
+                >
+                  <FontAwesomeIcon icon={faExclamation} />
+                </button>
+              </Tooltip>
+            ) : null}
+
+            {row.original.query && row.original.query.status === 'CLOSED' ? (
+              <Tooltip label="View closed query">
+                <button
+                  onClick={() => {
+                    setSelectedRow(row.original)
+                    setOpened(true)
+                  }}
+                >
+                  <FontAwesomeIcon icon={faCheck} />
+                </button>
+              </Tooltip>
+            ) : null}
+
+            {!row.original.query ? (
+              <Tooltip label="Create query">
+                <button
+                  onClick={() => {
+                    setSelectedRow(row.original)
+                    setOpened(true)
+                  }}
+                >
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
+              </Tooltip>
+            ) : null}
           </div>
         ),
       },
     ],
-    [],
-  );
+    []
+  )
 
   const table = useMantineReactTable({
     columns,
     data: form, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-  });
+  })
 
   return (
     <>
@@ -101,19 +133,16 @@ export default function Home() {
       >
         {selectedRow && (
           <Stack>
-            
-            <Text>query status</Text>
-            <Text>created on</Text>
-            <Text><strong>Queries:</strong> {selectedRow.queries}</Text>
-            <button >
-          Update Query 
-          </button>
+            <Text>{selectedRow.query?.status}</Text>
+            <Text>{selectedRow.query?.createdAt}</Text>
+            <Text>
+              <strong>Queries: {selectedRow.query?.title}</strong>
+            </Text>
+            <button>Update Query</button>
             <Button onClick={() => setOpened(false)}>Close</Button>
           </Stack>
         )}
       </Modal>
     </>
-  );
+  )
 }
-  
-
